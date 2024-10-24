28e5055af7e4857f8ef1fa3de300a7b1780aa979
jhorman villanueva <jhorman.villanueva@unad.edu.co>
2024-10-24 16:30:34 +0000

    update index

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 if __name__=='__main__':
-    app.run('0.0.0.0', port=5002)
+    app.run('0.0.0.0', debug=True, port=5002)
